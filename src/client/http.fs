7f8458942521a8f37d20ff07aa685fb69823e5a5
Dominik Kotecki <dominikus1910@gmail.com>
2016-12-26 18:46:46 +0100

    add http utilities

---
+++ src/client/http.fs
@@ -0,0 +1,28 @@
+module Http
+open System
+open Fable.Core
+open Fable.Import
+open Fable.Import.Browser
+
+type HttpMethod<'a> = 
+    | Get of url: string
+    | Post of url: string * data: 'a
+
+
+let request meth onSuccess onError =
+    let url, http, data =
+        match meth with
+        | Get url -> url, "GET", None
+        | Post (url, data) -> url, "POST", Some(JS.JSON.stringify data)
+    
+    let req = XMLHttpRequest.Create()
+
+    req.onreadystatechange <- fun _ ->
+        if req.readyState = 4. then
+            match req.status  with
+            | 200. | 0. -> JS.JSON.parse req.responseText |> unbox |> onSuccess
+            | _ -> req.statusText |> onError
+        null
+    
+    req.``open``(http, url, true)
+    req.send(data)
